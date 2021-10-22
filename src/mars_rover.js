e8a0d0811729bc9c3a5ba2305c7f75094b2c0c6c
albertoconcovia <concovia@gmail.com>
2021-10-22 14:44:52 +0100

    First Refactoring mars_rover.js and mars_rover.test.js

--- src/mars_rover.js
+++ src/mars_rover.js
@@ -1,8 +1,51 @@
+const mars_rover = (setMarsRoverInstructions) => {
+  // set initial rover position
+  const currentRoverPosition =
+    setMarsRoverInstructions.roverSetInstructions.roverPosition;
+  let currentRoverOrientation =
+    setMarsRoverInstructions.roverSetInstructions.roverPosition
+      .roverOrientation;
 
-const mars_rover = (coordinatesPlateau, roverSetInstructions) => {
+  // read rover instructions and iterate each instruction
+  [...setMarsRoverInstructions.roverSetInstructions.roverInstruction].forEach(
+    (instruction) => {
+      // If instruction is L or R we update the Orientation
+      if (instruction === "L" || instruction === "R") {
+        //roverX: 0, roverY: 2  O=S  instruction=M
+        if (instruction === "L") {
+          switch (currentRoverOrientation) {
+            case "W":
+              currentRoverOrientation = "S";
+              break;
+            case "S":
+              currentRoverOrientation = "E";
+              break;
+            case "E":
+              currentRoverOrientation = "N";
+              break;
+            case "N":
+              currentRoverOrientation = "W";
+              break;
+            default:
+              break;
+          }
+        }
+      }
 
-    return [1, 3, "N"];
-  };
-  
-  module.exports =  mars_rover ;
-  
+      // If instruction is M we update the Position
+      if (instruction === "M") {
+        if (currentRoverOrientation === "W") currentRoverPosition.roverX -= 1;
+        if (currentRoverOrientation === "S") currentRoverPosition.roverY -= 1;
+        if (currentRoverOrientation === "E") currentRoverPosition.roverX += 1;
+        if (currentRoverOrientation === "N") currentRoverPosition.roverY += 1;
+      }
+    }
+  );
+  const finalArrayRoverPosition = [];
+  finalArrayRoverPosition[0] = currentRoverPosition.roverX;
+  finalArrayRoverPosition[1] = currentRoverPosition.roverY;
+  finalArrayRoverPosition[2] = currentRoverOrientation;
+  return finalArrayRoverPosition;
+};
+
+module.exports = mars_rover;
--- src/mars_rover.test.js
+++ src/mars_rover.test.js
@@ -17,12 +17,19 @@ describe("Mars Rover - Move Around the Plateau test", () => {
   test("Given instructions to move - then return output successful ", () => {
 
     // Arrange
-    const coordinatesPlateau = { x: 5, y: 5 };
-    const roverSetInstructions = [ [1, 2, "N"],["LMLMLMLMM"]]
+    const setMarsRoverInstructions = {
+      plateauMaxCoordinates : { plateauX: 5, plateauY: 5 },
+      roverSetInstructions : {
+        roverPosition : {roverX: 1, roverY: 2, roverOrientation: "N"} ,
+        roverInstruction : "LMLMLMLMM"
+      }
+    };
+    // const coordinatesPlateau = { x: 5, y: 5 };
+    // const roverSetInstructions = [ [1, 2, "N"],["LMLMLMLMM"]]
     const result = [1, 3, "N"];
 
     // Act
-    const roverFinalPosition = mars_rover(coordinatesPlateau, roverSetInstructions);
+    const roverFinalPosition = mars_rover(setMarsRoverInstructions);
 
     // Assert
     expect(roverFinalPosition).toStrictEqual(result);
